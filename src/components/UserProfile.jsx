e56d252b353c5ae489c740f1a6831b31d537715a
whos who <whoswho@gmail.com>
2023-12-19 22:09:07 +0530

    blah

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -29,7 +29,6 @@ const UserProfile = () => {
         />
         <div>
           <p className="font-semibold text-xl dark:text-gray-200"> Deepak Mishra </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
           <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> deepak@gmail.com </p>
         </div>
       </div>
