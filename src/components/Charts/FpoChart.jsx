a6c36896e24d9c4bc513be2216da59a6fc68af01
Ambesh <ambeshgaunker@gmail.com>
2023-12-20 08:53:32 +0530

    Fixes

--- src/components/Charts/FpoChart.jsx
+++ src/components/Charts/FpoChart.jsx
@@ -10,27 +10,6 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-];
-
 const FpoChart = ({ yieldValue, predictedPrice, updatedAvgCredibility }) => {
   const data = [
     {
@@ -46,7 +25,7 @@ const FpoChart = ({ yieldValue, predictedPrice, updatedAvgCredibility }) => {
       amt: predictedPrice,
     },
     {
-      name: 'Updated Credibility',
+      name: 'Credibility',
       uv: updatedAvgCredibility,
       pv: updatedAvgCredibility,
       amt: updatedAvgCredibility,
